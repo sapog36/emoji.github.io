6c3c02b05508f09bcc3472abfe918452b8c84b4b
sapog36 <104499367+sapog36@users.noreply.github.com>
2022-10-24 12:32:57 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -10,7 +10,12 @@ let getJsonFile = (pathToFile) => {
   };
   const allEmoji = getJsonFile("https://emoji-api-app.herokuapp.com/"); 
   let divpos = document.getElementById('divpos');
- function draw(massiv) {
+  /*Возвращает ссылку на элемент по его идентификатору (ID);
+   идентификатор является строкой, которая может быть использована для идентификации элемента;
+   она может быть определена при помощи атрибута id в HTML или из скрипта.*/
+ 
+ 
+  function draw(massiv) {
   let str = '';
   divpos.innerHTML = '';
   for(let i = 0; i < massiv.length; i++){
@@ -22,13 +27,24 @@ let getJsonFile = (pathToFile) => {
       </div>`;
   }
   divpos.innerHTML = str;
+  /*Свойство интерфейса Element innerHTML устанавливает или получает HTML или XML разметку дочерних элементов.*/
  }
 
   draw (allEmoji);
 
-  document.querySelector('#search').oninput = function(){
+  document.querySelector('#search').oninput = function()  
+  /*Метод возвращает первый Documentэлемент в документе, который соответствует указанному селектору или группе селекторов.
+  Если совпадений не найдено, возвращается. querySelector()Element ноль*/  
+  {
     let value = this.value.trim();
+    /*функция this которое в нестрогом режиме всегда является ссылкой на объект, а в строгом режиме может принимать любое значение.*/
+
+    /*Метод trim() удаляет пробельные символы с начала и конца строки.*/
+
     newEmoji = allEmoji.filter(emoji => emoji.title.includes(value));
+    /*Метод filter() создаёт новый массив со всеми элементами, прошедшими проверку, задаваемую в передаваемой функции.*/
     draw(newEmoji);
+
   }
+  
   
